0c64600092cd219c79fb1f888e59e492cbce9f5e
Anurag-Dashore-123 <adashore564@gmail.com>
2025-05-29 14:40:05 +0530

    Update Image_pro_code.py

--- Image_pro_code.py
+++ Image_pro_code.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 
 # Load the image
-img = cv2.imread('image.jpg')
+img = cv2.imread('images (1).jpeg')
 
 # Convert the image to grayscale
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
